e9cd0a21e6ef0ce2ac4e8e909cff45691b576a0b
Yiwen Wang <wangyiwenau@gmail.com>
2025-05-29 10:47:36 +1000

    add central error handler

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+export const errorHandler = (error, req, res, next) => {
+  console.error(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message, data });
+};
