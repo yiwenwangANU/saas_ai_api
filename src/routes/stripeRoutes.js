d1b586b5ab499865dd696270cbf40d973805d558
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-15 11:00:23 +1000

    update stripe session success url

--- src/routes/stripeRoutes.js
+++ src/routes/stripeRoutes.js
@@ -21,7 +21,7 @@ router.post("/create-checkout-session", async (req, res) => {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.CLIENT_URL}/dashboard?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.CLIENT_URL}/subscribe_success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.CLIENT_URL}/cancel`,
     });
 
